bf5726f96f02e0549c8803ae147ae6bf61b72ab4
Angelo-Santiago <angelosantiago410@gmail.com>
2024-08-05 21:39:06 -0300

    progredindo no projeto

--- Author/models.py
+++ Author/models.py
@@ -2,7 +2,13 @@ from django.db import models
 
 from utils.base_models import BaseModel
 
-class Author(BaseModel):
-    nome = models.Charfield(
+class Authors(BaseModel):
+    nome = models.CharField(
         max_length=50, verbose_name="Nome do Autor"
     )
+    class Meta:
+        db_table = 'Autores'
+
+    def __str__(self):
+        return self.nome
+
--- Publication/models.py
+++ Publication/models.py
@@ -1,8 +1,20 @@
 from django.db import models
+from Author.models import Authors
+from datetime import date
 from utils.base_models import BaseModel
-from Author.models
 
-class Publication(BaseModel):
-    author = models.Charfield(
-        max_length=50, verbose_name="Nome da Publicação"
+class Publications(BaseModel):
+    date_pub = models.DateField(default=date.today)
+    author = models.ForeignKey(Authors, on_delete=models.CASCADE)
+    pub_text = models.CharField(
+        max_length=50, verbose_name="Texto da Publicação"
     )
+    pub_title = models.CharField(
+        max_length=30, verbose_name="Título da Publicação"
+    )
+
+    class Meta:
+        db_table = 'publicações'
+
+    def __str__(self):
+        return self.pub_title
